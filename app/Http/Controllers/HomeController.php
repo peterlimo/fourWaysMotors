cbcaffc1a361b89d9bd76f51bf0f73725d1d53da
josephmwangiG <josephmwangi.jgm@mail.com>
2021-10-18 11:13:26 +0300

    New application theme

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
-    //
-    public function ourNews()
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
-        return view("our-news");
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('dashboard');
     }
 }
